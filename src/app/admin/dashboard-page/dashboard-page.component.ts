f642b32660c58dd9d32b64e3a0dbe0c21bfc0721
Alexandr <XoxlofAlexandr@yandex.ru>
2021-02-25 03:34:12 +0300

    реализован вывод списка постов

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../shared/interfaces';
+import { PostsService } from '../shared/posts.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+  styleUrls: ['./dashboard-page.component.scss'],
 })
 export class DashboardPageComponent implements OnInit {
+  public posts: Observable<Post[]>;
 
-  constructor() { }
+  constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
+    this.posts = this.postsService.getAll();
   }
 
+  remove(id: string): void {}
 }
